25888fb192004b5e3d19b810320b0e9b9438e489
Daniel Agudelo <daniel.agudelo185@gmail.com>
2023-11-05 18:43:40 -0500

    SearchBar's clear

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { FaSistrix } from "react-icons/fa";
 const SearchBar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+    }
+    setSearchTerm('')
   };
+  const handleChage = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  
   return (
     <div className="relative ">
       <form
@@ -16,11 +26,15 @@ const SearchBar = () => {
         <input
           className="text-k1-black outline-none focus:outline-none placeholder:italic rounded-full px-4 pr-12 py-1 shadow-sm w-full "
           placeholder="Search"
+          value={searchTerm}
+          onChange={handleChage}
+          
         />
         <button
           id="search"
           name="search"
           className="absolute right-12 top-1/2 -translate-y-1/2"
+          
         >
           <FaSistrix className="text-2xl text-k4-black"></FaSistrix>
         </button>
